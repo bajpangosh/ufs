f1e9dbf154965dbf5ec520bbd36fc39d52bad468
Bajpan Gosh <bajpangosh@gmail.com>
2022-04-27 19:06:35 +0530

    Update nodeinstall.sh

--- nodeinstall.sh
+++ nodeinstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # GET ALL USER INPUT
-echo "Domain Name (eg. example.com)?"
+echo "App Name (eg. example google)?"
 read DOMAIN
-echo "App name PORT (8085)"
+echo "App PORT (8085)"
 read APP_PORT
-tput setaf 2; echo 'Wellcome to Parse Server and Dashboard on Ubuntu 18.04 install bash script';
+tput setaf 2; echo 'Wellcome to Node Server and Dashboard on Ubuntu 20.04 install bash script';
 sleep 2;
 tput sgr0
 cd ~
@@ -12,11 +12,34 @@ tput setaf 2; echo 'installing Node Js and Nginx Server';
 sleep 2;
 tput sgr0
 cd /etc/nginx/sites-available/
-sudo wget -O "app.$DOMAIN" https://goo.gl/2H3uGq
-sudo sed -i -e "s/app.example.com/app.$DOMAIN/" "app.$DOMAIN"
+sudo wget -O "app.$DOMAIN.adat.in" https://raw.githubusercontent.com/bajpangosh/ufs/main/apidomain.conf
+sudo sed -i -e "s/apidomain.adat.in/app.$DOMAIN.adat.in/" "app.$DOMAIN.adat.in"
+sudo sed -i -e "s/3569/$PORT/" "app.$DOMAIN.adat.in"
 
-sudo wget -O "dash.$DOMAIN" https://goo.gl/VZhPLP
-sudo sed -i -e "s/dash.example.com/dash.$DOMAIN/" "dash.$DOMAIN"
+sudo wget -O "$DOMAIN.adat.in" https://raw.githubusercontent.com/bajpangosh/ufs/main/rootdomain.conf
+sudo sed -i -e "s/rootdomain.adat.in/rootdomain.$DOMAIN.adat.in/" "$DOMAIN.adat.in"
 
-sudo ln -s /etc/nginx/sites-available/"app.$DOMAIN" /etc/nginx/sites-enabled/
-sudo ln -s /etc/nginx/sites-available/"dash.$DOMAIN" /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/"app.$DOMAIN.adat.in" /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/"$DOMAIN.adat.in" /etc/nginx/sites-enabled/
+sudo mkdir -p /var/www/"$DOMAIN.adat.in"
+sudo mkdir -p /projects/"app.$DOMAIN.adat.in"
+echo
+echo
+tput setaf 2; echo "Here is your Credentials"
+echo "--------------------------------"
+echo "APP_PORT:   $APP_PORT"
+echo
+echo
+echo "App:        https://app.$DOMAIN.adat.in"
+echo "Web:        https://$DOMAIN.adat.in"
+echo
+echo "--------------------------------"
+tput sgr0
+echo
+echo
+tput setaf 3;  echo "Installation & configuration succesfully finished."
+echo
+echo "Twitter @TeamKloudboy"
+echo "E-mail: support@kloudboy.com"
+echo "Bye! Your boy KLOUDBOY!"
+tput sgr0
